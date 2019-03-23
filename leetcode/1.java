deb0d28f3b5b84b85600cf0223c87f4a5baceb7d
skybirdbao <2810224786@qq.com>
2019-03-23 11:06:20 +0800

    add 1.java

--- leetcode/1.java
+++ leetcode/1.java
@@ -1,12 +1,17 @@
-  public int[] twoSum(int[] nums,int target) {
+import java.util.HashMap;
+import java.util.Map;
+
+public class One {
+     public int[] twoSum(int[] nums,int target) {
     	 Map<Integer,Integer> map = new HashMap<Integer,Integer>();
     	 for(int i=0;i<nums.length;i++) {
     		 map.put(nums[i], i);
     	 }
     	 for(int i=0;i<nums.length;i++) {
-    		 if(map.containsKey(target-nums[i])&& map.get(target-nums[i])!=i) {
+    		 if(map.containsKey(target-nums[i])) {
     			 return new int[] {i,map.get(target-nums[i])};
     		 }
     	 }
     	 throw new IllegalArgumentException("no sulation");
      }
+}
