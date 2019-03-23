44f8606107245705446db4ea044ff51db4380100
skybirdbao <2810224786@qq.com>
2019-03-23 11:12:46 +0800

    modify 1.java

--- leetcode/1.java
+++ leetcode/1.java
@@ -8,7 +8,7 @@ public class One {
     		 map.put(nums[i], i);
     	 }
     	 for(int i=0;i<nums.length;i++) {
-    		 if(map.containsKey(target-nums[i])) {
+    		 if(map.containsKey(target-nums[i])&& map.get(target-nums[i])!=i) {
     			 return new int[] {i,map.get(target-nums[i])};
     		 }
     	 }
